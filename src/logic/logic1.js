5406bced63fb57c67f9a8d4a3a2c3d76aa9a8705
Praveen Kumar Sharma <41022521+pravinsharma@users.noreply.github.com>
2021-10-10 21:04:01 +0530

    Create logic1.js

---
+++ src/logic/logic1.js
@@ -0,0 +1,7 @@
+if (name != ‘George’ && city != ‘Milan’) {
+	// some statements
+} else if (name == ‘George’ || city == ‘Milan’) {
+	// other statements
+} else { // un-reachable code which the compiler will not warn of
+	// un reachable code
+}
